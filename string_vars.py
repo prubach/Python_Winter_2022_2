26a23c413154f7abaaf16957da5a1b9a2cfb56d2
prubac <pawel.rubach@sgh.waw.pl>
2022-11-03 12:09:48 +0100

    String variables

--- string_vars.py
+++ string_vars.py
@@ -10,3 +10,12 @@ s2 = s1[:2] + s1[-2:]
 print('s2: {}'.format(s2))
 
 print(s1.find('fg'))
+
+#print('abc\ndefe\tgfvbhehe')
+print('abc\\ndefe\\tgfvbhehe')
+print('a\'bc\\ndefe\\tgfv"bhe"he')
+print("a'bc\ndefe\\tgfv\"bhe\"he")
+print('łóżżąąśćśćfqfeqgf')
+print(ord('a'))
+print(ord('ł'))
+print(chr(98))
