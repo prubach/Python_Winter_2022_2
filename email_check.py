594eb7cc8cc4e797b82a8488c363da89ea107f39
prubac <pawel.rubach@sgh.waw.pl>
2022-11-10 12:01:30 +0100

    Email checking for @ position and number of occurences

---
+++ email_check.py
@@ -0,0 +1,27 @@
+email1 = 'pawel.rubach@sgh.waw.pl'
+email2 = 'pawel.rubach#sgh.waw.pl'
+email3 = '@sgh.waw.pl'
+email4 = 'pawel@'
+email5 = 'pawel@.pl'
+email6 = 'pa@wel@wwa.pl'
+email7 = 'ab@sgh@waw@pl'
+
+
+def check_email(email):
+    if not email:
+        return False
+    # TODO implement different checks
+    if not (email.find('@') > 0 and email.find('@') < len(email)-1):
+        print('@ not found or in wrong position')
+        return False
+    if not (email.find('@') == email.rfind('@')):
+        print('@ too many of them')
+        return False
+    return True
+
+
+for em in [email1, email2, email3, email4, email5, email6, email7]:
+    print('{}: {}'.format(em, check_email(em)))
+
+#for i in range(len(email4)):
+#    print(email4[i])
