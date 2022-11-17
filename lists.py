3f15c9fb3606d5eb67a88cac3a83610b4c11cdae
prubac <pawel.rubach@sgh.waw.pl>
2022-11-17 12:10:17 +0100

    Lists - exercises solved

--- lists.py
+++ lists.py
@@ -23,4 +23,23 @@ for val in l1:
 
 l4 = [321, 26, 23163, 'Hello from Python 3', 3267, 89]
 #TODO extract the "Python" string from the list l4
+python_string_index = l4[3].find("Python")
+python_string = l4[3][python_string_index:python_string_index+6]
+print(python_string)
+
 #TODO sum up all numbers from l4  (type - check if it is a number)
+sum_of_numbers = 0
+for i in range(len(l4)):
+    if type(l4[i])==int:
+        sum_of_numbers += l4[i]
+print(sum_of_numbers)
+
+print('-----------')
+l5 = [x for x in l4 if isinstance(x, (int, float, complex))]
+print(l4)
+print(l5)
+print(sum(l5))
+
+l6 = [x*3 for x in l5]
+print(l6)
+
