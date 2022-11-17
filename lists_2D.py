460ccbd2b77403d7ddaa5c1fe2814fc9bf316ae2
prubac <pawel.rubach@sgh.waw.pl>
2022-11-17 12:30:30 +0100

    2D lists

---
+++ lists_2D.py
@@ -0,0 +1,15 @@
+l1 = [[1, 6, 7],
+      [8, 9, 3],
+      [7, 6, 2]]
+print(l1)
+l1_0 = l1[0]
+l1_1 = l1[1]
+l1_2 = l1[2]
+print(l1_1[2])
+print(l1[1][2])
+
+#TODO sum rows in l1
+#TODO sum columns in l1
+#TODO sum all elements of l1
+
+
