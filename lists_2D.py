1c78020e434f0a3945d229e8428280196e538064
prubac <pawel.rubach@sgh.waw.pl>
2022-11-17 13:02:08 +0100

    2D lists - summing solved

--- lists_2D.py
+++ lists_2D.py
@@ -8,8 +8,29 @@ l1_2 = l1[2]
 print(l1_1[2])
 print(l1[1][2])
 
-#TODO sum rows in l1
-#TODO sum columns in l1
-#TODO sum all elements of l1
 
+print('--------------')
+#TODO sum rows in l1
+for i in range(len(l1)):
+    print(sum(l1[i]))
+print('--------------')
+
+row_sum = []
+for row in l1:
+    print(sum(row))
+    row_sum.append(sum(row))
+
+print(row_sum)
+
+res = [sum(idx) for idx in l1]
+print(res)
+
+#TODO sum columns in l1
+
+
+res = [sum(idx) for idx in zip(*l1)]
+print(res)
+
+#TODO sum all elements of l1
+print(sum(res))
 
