48fe9645a12b7e10ed97a8f70ccf3143aa940831
prubac <pawel.rubach@sgh.waw.pl>
2022-11-24 12:27:06 +0100

    Read csv file

---
+++ file_read_csv.py
@@ -0,0 +1,17 @@
+my_file = 'my_matrix.csv'
+
+m = []
+with open(my_file, 'r') as f:
+    lines = f.readlines()
+    i = 0
+    for line in lines:
+        r = []
+        i += 1
+        print(f'{i}: {line}', end='')
+        for c in line.split(';'):
+            #print(type(c))
+            #print(c)
+            r.append(int(c))
+        m.append(r)
+
+print(m)
