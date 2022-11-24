f3fa185e2cc36bdc0a26752e712dcc86a12f9e0f
prubac <pawel.rubach@sgh.waw.pl>
2022-11-24 13:11:34 +0100

    Write and read as json

--- file_read_json.py
+++ file_read_json.py
@@ -1,5 +1,7 @@
 import json
 
+my_out_file = r'c:\users\prubac\Desktop\my_matrix_out.json'
+my_out_file = 'c:/users/prubac/Desktop/my_matrix_out.json'
 my_out_file = 'my_matrix_out.json'
 with open(my_out_file, 'r') as f:
     m = json.load(f)
