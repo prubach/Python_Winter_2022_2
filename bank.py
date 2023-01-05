005c8670f12564a85e368b27f9d6639396928b56
prubac <pawel.rubach@sgh.waw.pl>
2023-01-05 11:55:28 +0100

    Account with deposit and charge

--- bank.py
+++ bank.py
@@ -11,18 +11,31 @@ class Customer:
 
 
 
-
-
 class Account:
-    last_id = 0
+    last_id = 1000
     def __init__(self, customer):
         Account.last_id += 1
-        self.id = id
+        self.id = Account.last_id
         self.customer = customer
         self._balance = 0
+
+    def deposit(self, amount):
+        self._balance += amount
+
+    def charge(self, amount):
+        self._balance -= amount
+
+    def __repr__(self):
+        return f'Account [{self.id}, {self.customer.lastname}, {self._balance}]'
+
 
 
 c1 = Customer('John', "Smith")
 print(c1)
 
 a = Account(c1)
+print(a)
+a.deposit(100)
+print(a)
+a.charge(50)
+print(a)
